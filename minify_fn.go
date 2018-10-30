e0e7f14e703aa975c7bb43c5e85b3f542167f7e7
Theo Sorriaux <you@example.com>
2018-10-30 15:58:10 +0100

    minify update

--- minify_fn.go
+++ minify_fn.go
@@ -15,12 +15,15 @@ func minify_searchForString(s_file string, delimiters []string)(strings []String
 	var stack []string
 	in_string := false
 
-	for _, char := range s_file {
+	for index, char := range s_file {
 		is_delimiter := HasElem(delimiters,string(char))
 		if is_delimiter {
 			if len(stack) > 1  && stack[len(stack)-2] != "\\"{
 				if in_string{
-					
+					strings[len(strings)-1].End = index
+				}else{
+					strings = append(strings,StringInFile{})
+					strings[len(strings)-1].Begin = index
 				}
 				in_string = !in_string
 			}
