d85b312fa8063f01930dec98743481078745b13a
Philiphil <171012@supinfo.com>
2018-10-31 17:40:45 +0100

    minify++

--- main.go
+++ main.go
@@ -79,4 +79,3 @@ func executeTrigger(T Trigger){
 		}
 	}
 }
-
--- minify_fn.go
+++ minify_fn.go
@@ -51,9 +51,19 @@ func minify_searchForString(s_file string, delimiters []string)(strings []String
 }
 
 func minify_filter(s_file string, protected []StringInFile, delimiters Delimiter)(f_sf string, strings []StringInFile){
+	last_delimiter_index := 0
+	is_in_delimiter := false
+	for index, char := range s_file {
+		s_char := string(char)
+		if s_char == string(delimiters.Begin[len(delimiters.Begin)-1]){
+			//actual char might be delimiter
+			//must verify if delimiter is complete and if 
+		}
+
+	}
 	return
 }
 
-func minify_extract(strings []string, str string)(rstrings []string, rstr string){
-	return
+func minify_extract(strings []string, str string)([]string,  string){
+	return strings[:len(strings)-1], str+strings[len(strings)-1]
 }
